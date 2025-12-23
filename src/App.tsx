5b45cd546f0d5c9401fa88169d061b353e176bd4
Erfan Forouzi <erfanforouzzziii@gmail.com>
2025-12-23 16:24:03 +0330

    add header branch/header

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+    <header>
+      Header
+    </header>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
